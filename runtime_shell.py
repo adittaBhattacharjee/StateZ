64040ff500c89fc40582242a29f92d368b373181
Aditya Bhattachrajee <aditya2009student@gmail.com>
2022-12-31 11:13:53 +0600

    Initial Commit

---
+++ runtime_shell.py
@@ -0,0 +1,14 @@
+import lexer_and_parser as program
+
+while True:
+    console = input("$stateZ > ")
+    if console.strip() == "": continue
+    result, error = program.run("<console>", console)
+
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
